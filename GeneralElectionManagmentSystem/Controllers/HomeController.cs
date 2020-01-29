0a247ef05eae37c88cf80b8319081aefaefd44e3
html.html.101@gmail.com <html.html.101@gmail.com>
2020-01-29 13:58:11 +0500

    Add project files.

---
+++ GeneralElectionManagmentSystem/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralElectionManagmentSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Home_page()
+        {
+            ViewBag.Message = "My Home Page";
+            return View();
+        }
+        [Route
+            ("about us")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "My Home Page";
+            return View("About");
+        }
+
+        public ActionResult Elections()
+        {
+            return View();
+        }
+        public ActionResult for_Voters()
+        {
+            return View();
+        }
+        public ActionResult join_us_now()
+        {
+            return View();
+        }
+
+        public ActionResult parties_and_candidates()
+        {
+            return View();
+        }
+
+    }
+}
